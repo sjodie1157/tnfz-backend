4153f78f37e514968247d204b843e4259278d05d
sjodie1157 <sjodie1157@gmail.com>
2024-02-19 16:24:40 +0200

    added login

--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -7,6 +7,8 @@ router.get('/', controller.getUsers);
 router.get('/:id', controller.getOneUser);
 router.post('/addUser', controller.addUsers);
 router.patch('/updateUser/:id', controller.updateUser);
-router.delete('/removeUser/:id', controller.deleteUser);
+router.delete('/deleteUser/:id', controller.deleteUser);
+router.post('/signIn', controller.signIn);
 
 export default router;
+
